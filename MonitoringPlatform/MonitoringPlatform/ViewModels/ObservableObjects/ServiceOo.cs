8c91e29bf1eae2974618788f3a8ba895067b2dd2
Mechanical Object <mechanicalobject@gmail.com>
2015-01-17 14:51:52 +0100

    Added observable objects and automapper

---
+++ MonitoringPlatform/MonitoringPlatform/ViewModels/ObservableObjects/ServiceOo.cs
@@ -0,0 +1,44 @@
+﻿using GalaSoft.MvvmLight;
+using MonitoringPlatform.Models;
+
+namespace MonitoringPlatform.ViewModels.ObservableObjects
+{
+    public class ServiceOo : ObservableObject
+    {
+        private WindowsServiceStatus _status;
+        private string _serviceName;
+
+        public string ServiceName
+        {
+            get
+            {
+                return this._serviceName;
+            }
+            set
+            {
+                if (value == _serviceName)
+                    return;
+
+                this._serviceName = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        public WindowsServiceStatus Status
+        {
+            get
+            {
+                return this._status;
+            }
+            set
+            {
+                if (value == _status)
+                    return;
+
+                this._status = value;
+                this.RaisePropertyChanged();
+            }
+        }
+    }
+
+}
---
+++ MonitoringPlatform/MonitoringPlatform/ViewModels/ObservableObjects/UserOo.cs
@@ -0,0 +1,26 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace MonitoringPlatform.ViewModels.ObservableObjects
+{
+    public class UserOo : ObservableObject
+    {
+        private string _name;
+
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                if (value == _name)
+                    return;
+
+                this._name = value;
+                this.RaisePropertyChanged();
+            }
+        }
+    }
+
+}
