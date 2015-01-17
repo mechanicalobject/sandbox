a4e26cfa12d8d1b3acc1374a66bcd0775af45da3
Mechanical Object <mechanicalobject@gmail.com>
2015-01-17 14:45:25 +0100

    Added uni test project

---
+++ MonitoringPlatform/MonitoringPlatform.Tests/ServicesRepositoryTests.cs
@@ -0,0 +1,23 @@
+﻿using MonitoringPlatform.Repositories;
+using NUnit.Framework;
+
+namespace MonitoringPlatform.Tests
+{
+    [TestFixture]
+    public class ServicesRepositoryTests
+    {
+        [Test]
+        public void CheckThatGetServicesDoesNotReturnNull()
+        {
+            // Arrange
+            ServicesRepository subject = new ServicesRepository();
+
+            // Act
+            var result = subject.GetServices();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
---
+++ MonitoringPlatform/MonitoringPlatform.Tests/UsersRepositoryTests.cs
@@ -0,0 +1,23 @@
+﻿using MonitoringPlatform.Repositories;
+using NUnit.Framework;
+
+namespace MonitoringPlatform.Tests
+{
+    [TestFixture]
+    class UsersRepositoryTests
+    {
+        [Test]
+        public void CheckThatGetUsersDoesNotReturnNull()
+        {
+            // Arrange
+            UsersRepository subject = new UsersRepository();
+
+            // Act
+            var result = subject.GetUsers();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
